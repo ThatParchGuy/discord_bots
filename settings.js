1e402bf470cdeea746b48158909d85385cd0367c
ThatParchGuy <42625161+ThatParchGuy@users.noreply.github.com>
2018-09-06 23:12:51 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,2 +1,2 @@
-const yourID = "your id here";
-const botToken = "your bot token here";
+const yourID = "176765354465427458";
+const botToken = "NDg0NzU1OTE4ODIzNjIwNjI5.DmmpHA.zCBvYisechUxhYU3cZMUrUtZQOg";
